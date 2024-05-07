424edb83b6da31d30a8b75ac3fa264dd97999144
5Hyunwoo <158446938+5Hyunwoo@users.noreply.github.com>
2024-05-07 20:38:57 +0900

    hyunwoo
    8:38 hyunwoo

---
+++ eduwords/eduwords/src/join.js
@@ -0,0 +1,13 @@
+import React from "react";
+import  Navbar  from './main';
+import "./main.css";
+
+function Join() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default Join;
