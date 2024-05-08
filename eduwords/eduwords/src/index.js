7f1eace82c065af5fc0feea1b7b19beba558f602
5Hyunwoo <158446938+5Hyunwoo@users.noreply.github.com>
2024-05-08 15:14:44 +0900

    3:14
    123

--- eduwords/eduwords/src/index.js
+++ eduwords/eduwords/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Main from "./main";
 import Join from "./join";
+import Login from "./login";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -12,6 +13,7 @@ root.render(
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/join" element={<Join />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   </React.StrictMode>
---
+++ eduwords/eduwords/src/login.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Navbar from "./Component/Navbar";
+import "./login.css";
+import lg from "./img/logo.png";
+
+const login = () => {
+  return (
+    <div>
+      <Navbar />
+      <img src={lg} id="logoImg"></img>
+      <br></br>
+      <input type="text" name="username" placeholder="아이디" required />
+
+      <input
+        type="password"
+        name="password"
+        id="password"
+        placeholder="비밀번호"
+        required
+      />
+      <button onClick={""} id="loginbtn">
+        로그인
+      </button>
+      <br />
+      <br />
+      <div className="tool">
+        <ul>
+          <li>
+            <a href="findId">ID 찾기 </a>
+          </li>
+          <li>
+            <a href="findPw">PW 찾기 </a>
+          </li>
+          <li>
+            <a href="JoinId">회원가입</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default login;
