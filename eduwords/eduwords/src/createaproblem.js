cad5ea2d29f62f6bcb524e8c044b6fcfb2b39b5c
김하늘 <158141404+kimhaaneul@users.noreply.github.com>
2024-05-10 18:53:52 +0900

    18 53
    hn

--- eduwords/eduwords/src/createaproblem.js
+++ eduwords/eduwords/src/createaproblem.js
@@ -1,45 +1,103 @@
-import React from "react";
-import Navbar from "./Component/Navbar";
+import React, { useState } from "react";
+import NavbarT from "./Component/NavbarT";
 import "../src/createaproblem.css";
 
-const createaproblem = () => {
+const CreateAProblem = () => {
+  const [problemCount, setProblemCount] = useState(0); // 출제 문항 수 상태와 상태 변경 함수
+
+  // 문제 저장 버튼 클릭 시 출제 문항 수를 1 증가시키는 함수
+  const incrementProblemCount = () => {
+    setProblemCount((prevCount) => prevCount + 1);
+  };
+
   return (
     <div>
-      <Navbar />
+      <NavbarT />
       <h2 className="createproblem-title">· 직접 문제 생성</h2>
       <div>
         <table className="createproblem-table">
           <tbody>
             <tr>
-              <th className="createproblem-th" colSpan={2}>
+              <th className="createproblem-th1" colSpan={2}>
                 문제 유형
               </th>
-              <td>
-                <input type="radio" id="주관식" />
-                <label htmlFor="agree">주관식</label>
-                <input type="radio" id="객관식" />
-                <label htmlFor="disagree">객관식</label>
+              <td className="createproble-radio">
+                <input type="radio" className="Short" name="problemType" />
+                주관식
+                <input type="radio" className="choice" name="problemType" />
+                객관식
               </td>
             </tr>
+            <hr />
             <tr>
-              <th className="createproblem-th" colSpan={2}>
+              <th className="createproblem-th2" colSpan={2}>
+                난이도
+              </th>
+              <td>
+                <input type="number" placeholder="난이도를 선택하세요" />
+              </td>
+            </tr>
+            <hr />
+
+            <tr>
+              <th className="createproblem-th2" colSpan={2}>
                 출제문항수
               </th>
               <td>
+                <p className="createproblem-p">
+                  현재 {problemCount}문제가 저장되었습니다.
+                </p>
+              </td>
+            </tr>
+            <hr />
+            <tr>
+              <th className="createproblem-th3" colSpan={2}>
+                문제
+              </th>
+
+              <td>
                 <input
-                  type="text, number"
-                  type="number"
-                  name="createproblemnumber"
-                  placeholder="출제 문항 수를 입력하세요.(최대 25문제까지 가능합니다)"
+                  type="text"
+                  className="createproblem-questions"
+                  placeholder="문제를 입력해주세요."
                 ></input>
-                <button className="createproblem-btn">확인</button>
+                <input
+                  type="text"
+                  name="createproblem-a"
+                  placeholder="보기를 입력해주세요."
+                ></input>
+                <input type="radio" />
+                <input
+                  type="text"
+                  name="createproblem-a"
+                  placeholder="보기를 입력해주세요."
+                ></input>
+                <input type="radio" />
+                <input
+                  type="text"
+                  name="createproblem-a"
+                  placeholder="보기를 입력해주세요."
+                ></input>
+                <input type="radio" />
+                <input
+                  type="text"
+                  name="createproblem-a"
+                  placeholder="보기를 입력해주세요."
+                ></input>
+                <input type="radio" />
               </td>
             </tr>
           </tbody>
         </table>
+        <br></br>
+        <br></br>
+        <br></br>
+        <button className="createproblem-btn" onClick={incrementProblemCount}>
+          문제저장
+        </button>
       </div>
     </div>
   );
 };
 
-export default createaproblem;
+export default CreateAProblem;
