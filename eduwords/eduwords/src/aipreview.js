92f295c7bc2d48ccc41ed55f7d7fba0888e4f4e0
김하늘 <158141404+kimhaaneul@users.noreply.github.com>
2024-05-10 14:28:01 +0900

    14 27
    hn

--- eduwords/eduwords/src/aipreview.js
+++ eduwords/eduwords/src/aipreview.js
@@ -1,7 +1,32 @@
 import React from "react";
+<<<<<<< Updated upstream
 
 const aipreview = () => {
   return <div>aipreview</div>;
 };
 
 export default aipreview;
+=======
+import Navbar from "./Component/Navbar";
+import "../src/aipreview.css";
+
+const Aipreview = () => {
+  return (
+    <div>
+      <Navbar />
+      <h2 className="aipreview-title">· 생성된 문제 미리보기</h2>
+      <br></br>
+      <br></br>
+      <div className="aipreview-box"></div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div>
+        <button className="aipreview-btn">문제저장</button>
+      </div>
+    </div>
+  );
+};
+
+export default Aipreview;
+>>>>>>> Stashed changes
