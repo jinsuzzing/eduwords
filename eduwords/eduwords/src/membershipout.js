80620433a9a73bc2fbb7f988204a6b6ef8867371
김하늘 <158141404+kimhaaneul@users.noreply.github.com>
2024-05-11 15:51:35 +0900

    15 51
    hn

---
+++ eduwords/eduwords/src/membershipout.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const membershipout = () => {
+  return <div>회원탈퇴</div>;
+};
+
+export default membershipout;
--- eduwords/eduwords/src/vocabularynote.js
+++ eduwords/eduwords/src/vocabularynote.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import NavbarT from "./Component/NavbarT";
+import "../src/vocabularynote.css";
 
-const vocabularynote = () => {
-  return <div>단어장</div>;
+const VocabularyNote = () => {
+  const [wordSets, setWordSets] = useState([
+    { word: "apple", meaning: "사과" },
+    { word: "banana", meaning: "바나나" },
+    { word: "cat", meaning: "고양이" },
+    { word: "dog", meaning: "개" },
+  ]);
+
+  const addWordSet = (newWordSet) => {
+    setWordSets([...wordSets, newWordSet]);
+  };
+
+  return (
+    <div>
+      <NavbarT />
+      <h1 className="vocabularynote-title">· 단어장</h1>
+
+      <div className="vn-box">
+        <div className="vn-scroll">
+          <table>
+            <thead>
+              <tr>
+                <th className="vn-th1">단어</th>
+                <th className="vn-th2">뜻</th>
+                <th className="vn-th1">단어</th>
+                <th className="vn-th2">뜻</th>
+              </tr>
+            </thead>
+            <tbody>
+              {wordSets.map((wordSet, index) => (
+                <tr key={index}>
+                  <td className="vn-td-word">{wordSet.word}</td>
+                  <td className="vn-td-meaning">{wordSet.meaning}</td>
+                  <td className="vn-td-word">{wordSet.word}</td>
+                  <td className="vn-td-meaning">{wordSet.meaning}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default vocabularynote;
+export default VocabularyNote;
