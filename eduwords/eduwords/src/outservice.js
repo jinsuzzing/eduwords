ae74102283c99b26ea23d46bddaa0c44114debbe
5Hyunwoo <158446938+5Hyunwoo@users.noreply.github.com>
2024-05-13 12:15:59 +0900

    1215
    떙겨~~

--- eduwords/eduwords/src/outservice.js
+++ eduwords/eduwords/src/outservice.js
@@ -1,28 +1,46 @@
 import React from "react";
 import NavbarT from "./Component/NavbarT";
-import "./outservice";
+import "./outservice.css";
 import { Link } from "react-router-dom";
 
 const outservice = () => {
   return (
     <div>
       <NavbarT />
-      <div>
-        <h3>회원 탈퇴</h3>
-        <tr>탈퇴 후 회원정보 및 서비스 이용기록은 모두 삭제됩니다.</tr>
-        <tr>삭제된 데이터는 복구되지 않습니다.</tr>
+      <br />
+      <br />
+      <br />
+      <div className="outdiv">
+        <h1>회원 탈퇴</h1>
+        <tr id="outtr1">
+          탈퇴 후 회원정보 및 서비스 이용기록은 모두 삭제됩니다.
+        </tr>
+        <br />
+        <tr id="outtr2">삭제된 데이터는 복구되지 않습니다.</tr>
         <input
           type="text"
           name="username"
-          id="eraseID"
+          id="eraseId"
           placeholder="삭제하실 아이디 입력"
           required
         ></input>
         <br />
-        <input type="radio" />
-        예, 탈퇴에 동의합니다.
+        <input
+          type="text"
+          name="password"
+          id="erasepw"
+          placeholder="삭제하실 아이디의 비밀번호 확인"
+          required
+        ></input>
+        <br />
+        <br />
+        <input type="radio" id="outagree" name="agreement" />
+        <label htmlFor="outagree" className="radio-label">
+          예, 탈퇴에 동의합니다.
+        </label>
       </div>
-      <Link to="/outsuccess" component="button" className="outBtn">
+      <br />
+      <Link to="/outsuccess" component="button" className="container">
         탈퇴하기
       </Link>
     </div>
