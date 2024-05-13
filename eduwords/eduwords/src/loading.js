fdb7d14ec11c53aab842446854d2697e285bf5ad
5Hyunwoo <158446938+5Hyunwoo@users.noreply.github.com>
2024-05-13 18:02:12 +0900

    1802
    123

--- eduwords/eduwords/src/loading.js
+++ eduwords/eduwords/src/loading.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "./loading.css";
 import NavbarT from "./Component/NavbarT";
+import LoadingImg from "./img/loading.png";
 
-const loading = () => {
+const Loading = () => {
   return (
     <div>
       <NavbarT />
+      <br />
+      <br />
+      <div className="loadingdiv">
+        <img src={LoadingImg} className="loadingImg" alt="Loading" />
+        <br />
+        <br />
+        <b id="LoadingText">Loading..</b>
+      </div>
     </div>
   );
 };
 
-export default loading;
+export default Loading;
