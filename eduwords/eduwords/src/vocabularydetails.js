acf6dc76a4df225de3153ac37e6bf425c68bf4a3
김하늘 <158141404+kimhaaneul@users.noreply.github.com>
2024-05-13 16:31:41 +0900

    16 31
    hn

---
+++ eduwords/eduwords/src/vocabularydetails.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../src/vocabularydetails.css";
+
+const vocabularydetails = () => {
+  return (
+    <div>
+      <h2>dddd</h2>
+    </div>
+  );
+};
+
+export default vocabularydetails;
