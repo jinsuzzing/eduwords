5bfcdc431ed86d961dcd3bba4a7fa2f32d04832a
김하늘 <158141404+kimhaaneul@users.noreply.github.com>
2024-05-14 08:23:31 +0900

    08023
    hn

--- eduwords/eduwords/src/vocabularydetails.js
+++ eduwords/eduwords/src/vocabularydetails.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "../src/vocabularydetails.css";
+import NavbarT from "./Component/NavbarT";
+import { useParams } from "react-router-dom";
 
 const vocabularydetails = () => {
+  //   const { id } = useParams;
+
   return (
     <div>
-      <h2>dddd</h2>
+      <NavbarT />
+
+      <h1 className="vd-title">· 내 단어장</h1>
+
+      <div className="vd-box">{/* <p>ID: {id}</p> */}</div>
     </div>
   );
 };
