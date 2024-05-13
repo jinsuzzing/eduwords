7830d61039f5e1db5cf39df2e0becc33a60a6900
5Hyunwoo <158446938+5Hyunwoo@users.noreply.github.com>
2024-05-13 18:27:33 +0900

    1827

--- eduwords/eduwords/src/loading.js
+++ eduwords/eduwords/src/loading.js
@@ -9,11 +9,19 @@ const Loading = () => {
       <NavbarT />
       <br />
       <br />
+      <br />
       <div className="loadingdiv">
         <img src={LoadingImg} className="loadingImg" alt="Loading" />
         <br />
         <br />
         <b id="LoadingText">Loading..</b>
+        <br />
+        <br />
+        <table id="LoadingNow">
+          <tr id="Load1">문제 생성중입니다.</tr>
+          <br />
+          <tr id="Load2">잠시만 기다려주세요.</tr>
+        </table>
       </div>
     </div>
   );
