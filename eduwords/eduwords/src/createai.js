a63f9d5bdf1caa6e63660aa28f75f517fed26f04
5Hyunwoo <158446938+5Hyunwoo@users.noreply.github.com>
2024-05-17 16:49:29 +0900

    16 49
    hw

--- eduwords/eduwords/src/createai.js
+++ eduwords/eduwords/src/createai.js
@@ -2,8 +2,13 @@ import React from "react";
 import Navbar from "./Component/Navbar";
 import turboimg from "../src/img/gptturbo.png";
 import "../src/createai.css";
-
+import { Link } from "react-router-dom";
 const createai = () => {
+  const cantKeyDown = (e) => {
+    e.preventDefault();
+    // 이거쓰면 사용자가 키보드로 숫자 입력하는거 막을수 있음
+    // 위아래 버튼으로만 0~25까지 입력하게 하는거임
+  };
   return (
     <div>
       <Navbar />
@@ -15,9 +20,15 @@ const createai = () => {
           <th colSpan={2} className="gpttitle">
             문제유형
           </th>
-          <td>
-            <input type="radio" /> 주관식
-            <input type="radio" /> 객관식
+          <td className="maketd">
+            <tr>
+              <input type="radio" name="choiceQuestion" className="cq1" />{" "}
+              주관식
+            </tr>
+            <tr className="movetr">
+              <input type="radio" name="choiceQuestion" className="cq2" />{" "}
+              객관식
+            </tr>
           </td>
         </tr>
         <hr />
@@ -29,16 +40,18 @@ const createai = () => {
             <input
               type="number"
               className="gptinputbox"
-              placeholder="출제 문항 수를 입력해주세요.(최대 25문제까지 가능합니다)"
+              placeholder="최대 25문제까지 가능합니다"
+              min="0"
+              max="25"
+              onKeyDown={cantKeyDown}
             />
-            <button className="gptbtn1">확인</button>
           </td>
         </tr>
       </table>
       <div>
-        <button class="container" onClick={""}>
-          다음으로
-        </button>
+        <Link to="/aipreview">
+          <button className="createNext">다음으로</button>
+        </Link>
       </div>
     </div>
   );
