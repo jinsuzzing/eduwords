b73544ef750ec8f2b89bb4c2f5a7864408ee36cc
김진수 <80892395+subhun@users.noreply.github.com>
2024-05-21 12:03:58 +0900

    진수
    ㄹ

--- eduwords/eduwords/src/js/addword.js
+++ eduwords/eduwords/src/js/addword.js
@@ -16,7 +16,7 @@ const AddWord = () => {
 
   const handleTranslate = async () => {
     try {
-      const response = await axios.post("http://localhost:5002/translate", {
+      const response = await axios.post("http://localhost:8081/addword", {
         word,
       });
       if (response.data.translation) {
