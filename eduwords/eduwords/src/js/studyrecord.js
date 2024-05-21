a8cca8bd941810b059e5986d5d5f5c8c9e5b9ceb
김하늘 <158141404+kimhaaneul@users.noreply.github.com>
2024-05-21 15:57:11 +0900

    15 57
    hn

--- eduwords/eduwords/src/js/studyrecord.js
+++ eduwords/eduwords/src/js/studyrecord.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import "../css/studyrecord.css";
+import Navbar from "../Component/Navbar";
 
 const StudyRecord = () => {
   const location = useLocation();
@@ -8,8 +9,10 @@ const StudyRecord = () => {
   const gradingResults = location.state?.gradingResults || [];
   const score = location.state?.score || 0;
 
+  const selectedQuestions = examInfo.selectedQuestions || [];
+
   const renderIncorrectQuestions = () => {
-    return examInfo.selectedQuestions
+    return selectedQuestions
       .filter((question, index) => gradingResults[index] === "x")
       .map((question, index) => (
         <div key={index} className="incorrect-question">
@@ -21,21 +24,22 @@ const StudyRecord = () => {
 
   return (
     <div>
+      <Navbar />
       <h2 className="studdyrecord-title">· 공부기록</h2>
       <br />
-      <span>
-        시험 날짜: {examInfo.startDate} ~ {examInfo.endDate}
-      </span>
       <p className="sr-p">
-        · 내 점수: {score} / {examInfo.selectedQuestions.length}
+        시험 날짜: {examInfo.startDate} ~ {examInfo.endDate}
       </p>
+      <h3 className="sr-h3">
+        · 내 점수: {score} / {selectedQuestions.length}
+      </h3>
       <hr className="sr-hr" />
       <div className="sr-box1">
-        <p className="sr-p">· 틀린 문제 보기</p>
+        <h3 className="sr-h3">· 틀린 문제 보기</h3>
         <div className="mark-x-box">{renderIncorrectQuestions()}</div>
       </div>
       <br />
-      <p className="sr-p">· 최근 성적 한 눈에 보기</p>
+      <h3 className="sr-h3">· 최근 성적 한 눈에 보기</h3>
       <hr className="sr-hr" />
     </div>
   );
