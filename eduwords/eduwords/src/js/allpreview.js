ed8fa5b5b45ac1badf2b529eceff51ba561e2bae
김진수 <80892395+subhun@users.noreply.github.com>
2024-05-24 10:53:09 +0900

    10 52
    진수

--- eduwords/eduwords/src/js/allpreview.js
+++ eduwords/eduwords/src/js/allpreview.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "../css/allpreview.css";
 import NavbarT from "../Component/NavbarT";
 import Navbar from "../Component/Navbar";
@@ -25,20 +26,29 @@ const AllPreview = () => {
   const type = sessionStorage.getItem("mem_type");
 
   const handleConfirm = () => {
-    const newExam = {
+    const examInfo = {
       examName: examName,
       startDate: startDate,
       endDate: endDate,
-      selectedQuestions: selectedQuestions.map((question) => ({
-        ...question,
-        correctAnswer: question.correctAnswer,
+      selectedQuestions: selectedQuestions.map((q) => ({
+        content: q.content,
+        ex1: q.options?.ex1,
+        ex2: q.options?.ex2,
+        ex3: q.options?.ex3,
+        ex4: q.options?.ex4,
+        ex5: q.options?.ex5,
       })),
     };
 
-    const storedExamsInfo = JSON.parse(localStorage.getItem("examsInfo")) || [];
-    storedExamsInfo.push(newExam);
-    localStorage.setItem("examsInfo", JSON.stringify(storedExamsInfo));
-    navigate("/questionsok", { state: { examInfo: newExam } });
+    axios
+      .post("http://localhost:8081/getAll", examInfo)
+      .then((response) => {
+        navigate("/questionsok", { state: { examInfo: response.data } });
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error creating the exam!", error);
+      });
   };
 
   const handleBack = () => {
@@ -58,11 +68,11 @@ const AllPreview = () => {
                   <p>
                     {columnIndex * 2 + questionIndex + 1}. {question.content}
                   </p>
-                  <p>① {question.options?.option1}</p>
-                  <p>② {question.options?.option2}</p>
-                  <p>③ {question.options?.option3}</p>
-                  <p>④ {question.options?.option4}</p>
-                  <p>⑤ {question.options?.option5}</p>
+                  <p>① {question.options?.ex1}</p>
+                  <p>② {question.options?.ex2}</p>
+                  <p>③ {question.options?.ex3}</p>
+                  <p>④ {question.options?.ex4}</p>
+                  <p>⑤ {question.options?.ex5}</p>
                 </div>
               ))}
             </div>
--- eduwords/eduwords/src/js/studyroom.js
+++ eduwords/eduwords/src/js/studyroom.js
@@ -1,49 +1,75 @@
 import React, { useEffect, useState } from "react";
-import stb from "../img/stb1.png";
-import "../css/studyroom.css";
-import Navbar from "../Component/Navbar";
+import axios from "axios";
+import tb from "../img/tb.png";
+import "../css/questionslist.css";
 import NavbarT from "../Component/NavbarT";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
-const type = sessionStorage.getItem("mem_type");
-
-const StudyRoom = () => {
+const QuestionsList = () => {
   const navigate = useNavigate();
+  const currentLocation = useLocation();
   const [examsInfo, setExamsInfo] = useState([]);
+  const mem_id = sessionStorage.getItem("mem_id");
+
+  const formatDate = (dateString) => {
+    if (!dateString) return "";
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // 월은 0부터 시작하므로 1을 더합니다.
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  };
 
   useEffect(() => {
-    const storedExamsInfo = JSON.parse(localStorage.getItem("examsInfo")) || [];
-    const currentDate = new Date();
-    const validExams = storedExamsInfo.filter(
-      (exam) => new Date(exam.endDate) >= currentDate
-    );
-    setExamsInfo(validExams);
+    axios
+      .post("http://localhost:8081/getAll")
+      .then((response) => {
+        console.log(response.data); // 데이터 확인
+        const formattedExams = response.data.map((exam) => ({
+          ...exam,
+          startline: formatDate(exam.startline),
+          deadline: formatDate(exam.deadline),
+        }));
+        setExamsInfo(formattedExams);
+      })
+      .catch((error) => {
+        console.error("단어를 가져오는 중 오류 발생:", error);
+      });
   }, []);
 
   const handleTableClick = (exam) => {
-    navigate("/testpaper", { state: { examInfo: exam } });
+    navigate("/testpaper", {
+      state: {
+        selectedAnswers: currentLocation.state?.selectedAnswers,
+        examInfo: exam,
+      },
+    });
+  };
+
+  const handleEditClick = (exam) => {
+    navigate("/editexam", { state: { examInfo: exam } });
   };
 
   return (
     <div>
-      {type === "1" ? <NavbarT /> : <Navbar />}
-      <img src={stb} className="sbimg" alt="table"></img>
-      <div className="s-margin-box"></div>
+      <NavbarT />
+      <img src={tb} className="tbimg" alt="table"></img>
+      <div className="t-margin-box"></div>
       {examsInfo.map((exam, index) => (
         <table
           key={index}
-          className="s-listtable"
+          className="t-listtable"
           onClick={() => handleTableClick(exam)}
         >
           <tbody>
-            <tr className="s-listtable-tr1">
+            <tr className="t-listtable-tr1">
               <th colSpan={2}>
-                {exam.startDate} ~ {exam.endDate}
+                {exam.startline} ~ {exam.deadline}
               </th>
             </tr>
-            <tr className="s-listtable-tr2">
-              <td className="exam-name" colSpan={2}>
-                {exam.examName}
+            <tr className="t-listtable-tr2">
+              <td colSpan={2} className="exam-name">
+                {exam.work_name}
               </td>
             </tr>
           </tbody>
@@ -53,4 +79,4 @@ const StudyRoom = () => {
   );
 };
 
-export default StudyRoom;
+export default QuestionsList;
